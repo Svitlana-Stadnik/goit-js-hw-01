27fa2474e6652eb9fb00d2e82a557325364ca782
Svitlana Stadnik <svetlanasm@ukr.net>
2026-03-17 08:10:45 +0100

    add functions

--- js/task-2.js
+++ js/task-2.js
@@ -1,5 +1,10 @@
+function getShippingMessage(country, price, deliveryFee) {
+    const totalPrice = price + deliveryFee;
+    return (`Shipping to ${country}  will cost ${totalPrice} credits`);
+    
+}
 
-
+getShippingMessage();
 
 
 console.log(getShippingMessage("Australia", 120, 50)); // "Shipping to Australia will cost 170 credits"
